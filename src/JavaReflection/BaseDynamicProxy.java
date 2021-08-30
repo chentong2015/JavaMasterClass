040e5708a182c86112136bcefee50b6df1b75407
Tong Chen <tong.chen@etu.utc.fr>
2021-08-31 00:14:07 +0200

    to do java reflection and dynamic proxy

--- src/JavaReflection/BaseDynamicProxy.java
+++ src/JavaReflection/BaseDynamicProxy.java
@@ -1,7 +1,34 @@
 package JavaReflection;
 
-// Java三种代理模式：静态代理、动态代理和cglib代理 https://segmentfault.com/a/1190000011291179
-// TODO: 动态代理是如何实现的 ? https://www.jianshu.com/p/9bcac608c714
-public class BaseDynamicProxy {
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
 
+// TODO: 动态代理的实现原理以及AOP的底层
+// https://cloud.tencent.com/developer/article/1461796
+
+// Java源码提供的动态代理类型 Dynamic Proxy Classes，基于Java内部的反射来实现
+// java.lang.reflect.Proxy
+// https://docs.oracle.com/javase/7/docs/technotes/guides/reflection/proxy.html
+//
+// https://www.huaweicloud.com/articles/696efe7865fb093d4217487b29fc2506.html
+// https://bbs.huaweicloud.com/blogs/detail/191522
+// https://bbs.huaweicloud.com/blogs/215606
+//
+// Java中如果一个类型要实现动态代理，则必须实现InvocationHandler接口，实现invoke()方法
+public class BaseDynamicProxy implements InvocationHandler {
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        return null;
+    }
+
+    // proxy.newProxyInstance();
+    // 为特定的接口返回代理的实例对象，以此来分发(接口)中方法的调用给特定的"InvocationHandler"调用处理器
+    // Returns a proxy instance for the specified interfaces that dispatches method invocations to the specified invocation handler.
+    // public static Object newProxyInstance(ClassLoader loader, Class<?>[] interfaces, InvocationHandler h) {
+    //        Objects.requireNonNull(h);
+    //        final Class<?> caller = System.getSecurityManager() == null ? null: Reflection.getCallerClass();
+    //        Constructor<?> cons = getProxyConstructor(caller, loader, interfaces);
+    //        return newProxyInstance(caller, cons, h);
+    //    }
 }
