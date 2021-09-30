079e27f89303c886b7baa324a87045ae3707cfeb
Tong Chen <tong.chen@etu.utc.fr>
2021-09-30 13:18:46 +0200

    add java basic date time

--- src/JavaBasic/Base06DateTime.java
+++ src/JavaBasic/Base06DateTime.java
@@ -1,10 +1,10 @@
 package JavaBasic;
 
-import java.time.Clock;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
+import java.time.*;
 import java.time.format.DateTimeFormatter;
 import java.util.Calendar;
+import java.util.List;
+import java.util.stream.Collectors;
 
 // Java 8时间日期API:
 // LocalDate, LocalTime, LocalDateTime, Clock, Instant等类
@@ -29,5 +29,17 @@ public class Base06DateTime {
         LocalDate localDate = LocalDate.now();
         DateTimeFormatter newFormatter = DateTimeFormatter.ofPattern("yyyy/MM/dd");
         System.out.println(localDate.format(newFormatter));
+    }
+
+    // .datesUntil()
+    public List<LocalDate> getDatesBetweenUsingJava9(LocalDate startDate, LocalDate endDate) {
+        // 提取指定一个月中的所有天数
+        YearMonth month = YearMonth.of(2021, Month.JANUARY);
+        LocalDate firstOfMonth = month.atDay(1);
+        LocalDate firstOfFollowingMonth = month.plusMonths(1).atDay(1);
+        firstOfMonth.datesUntil(firstOfFollowingMonth).forEach(System.out::println);
+
+        // 提取两个Date之间的所有天数
+        return startDate.datesUntil(endDate).collect(Collectors.toList());
     }
 }
