42ff9875e20a3c8b7a8ba06dd2c7309bea8c28a3
Tong Chen <tong.chen@etu.utc.fr>
2021-11-01 20:44:40 +0100

    finish java class loader

--- java.virtual.machine/src/jvm_basics/chapter07_Jvm_ClassLoader/classloader/BaseClassLoader.java
+++ java.virtual.machine/src/jvm_basics/chapter07_Jvm_ClassLoader/classloader/BaseClassLoader.java
@@ -1,8 +1,8 @@
 package jvm_basics.chapter07_Jvm_ClassLoader.classloader;
 
 // 从JVM的角度看，只有两种类加载器
-// 1. 启动类加载器: Bootstrap ClassLoader(C++实现), 属于虚拟机的一部分 --> 显示出来是null
-// 2. 其他所有类加载器: 继承自抽象类ClassLoader, 由Java实现, 独立于虚拟机外部
+// 1. 启动类加载器: Bootstrap ClassLoader(C++实现), 属于虚拟机的一部分, 无法在java层面获取
+// 2. 其他类加载器: 继承自抽象类ClassLoader, 由Java实现, 独立于虚拟机外部
 
 // JDK 9之前，三层类加载器
 // 1. 启动类加载器: 加载存放在<JAVA_HOME>\lib目录中的(基础核心)类库，通过特定名称识别rt.jar, tools.jar
@@ -12,9 +12,13 @@ package jvm_basics.chapter07_Jvm_ClassLoader.classloader;
 public class BaseClassLoader {
 
     // "Class Loader类加载器"：通过一个类的全限定命来获取描述该类的二进制字节流
-    // 1. MyClass.class类之前还必须加载它的父类.class
-    // 2. TODO: 类本身和它的类加载器共同决定了在其虚拟机中的唯一性, 判断同名的class类型在同一个JVM下能否共存
-    // 3. "类相等"是指由同一个类加载器的前提下才有意义: equals(), isAssignableFrom(), isInstance()方法返回的结果必须一致
+    // MyClass.class类加载之前
+    // 1. 需要加载它的父类.class
+    // 2. 需要加载它使用到的相关package包中的class类
+    //
+    // TODO: 类本身和它的类加载器(ClassLoader的实例)共同决定了在其虚拟机中的唯一性,
+    //       判断同名的class类型在同一个JVM下能否共存
+    // "类相等"是指由同一个类加载器的前提下才有意义: equals(), isInstance()方法返回的结果必须一致
     public void testClassLoader() {
         System.out.println(String.class.getClassLoader()); // 拿到指定类型的类加载器
         // classLoader.getClass().getName();
--- java.virtual.machine/src/jvm_basics/chapter07_Jvm_ClassLoader/classloader/MyClassLoader.java
+++ java.virtual.machine/src/jvm_basics/chapter07_Jvm_ClassLoader/classloader/MyClassLoader.java
@@ -6,6 +6,7 @@ import java.io.InputStream;
 
 // TODO: 为什么要自定义类加载器 ?
 // 在某些特殊的场景下，需要加载网络上的资源文件，或者磁盘上指定文件路径下的文件
+// 这些class类文件无法通过父类的加载器来实现加载
 public class MyClassLoader extends ClassLoader {
 
     @Override
@@ -23,7 +24,7 @@ public class MyClassLoader extends ClassLoader {
         }
     }
 
-    // 自定在什么位置(路径)下，通过名称找到class文件
+    // 自定义在什么位置路径下，通过名称找到class文件
     // 然后根据从class文件中读取的数据，加载到内存并生成Class<?>
     @Override
     protected Class<?> findClass(String name) throws ClassNotFoundException {
@@ -41,6 +42,7 @@ public class MyClassLoader extends ClassLoader {
         String classPath = "...";
         String formattedName = name.replaceAll("\\.", "/");
         String classFilePath = classPath + "/" + formattedName + ".class";
+        // 将IO Stream流的信息读到字节数组中
         FileInputStream inputStream = new FileInputStream(classFilePath);
         int size = inputStream.available();
         byte[] data = new byte[size];
