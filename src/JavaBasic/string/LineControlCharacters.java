9ed0bba151aa8f15647649208472ba2076c306ef
tchen <tchen@murex.com>
2023-01-27 10:34:59 +0100

    refresh java basic string

--- src/JavaBasic/string/LineControlCharacters.java
+++ src/JavaBasic/string/LineControlCharacters.java
@@ -10,38 +10,53 @@ package JavaBasic.string;
 public class LineControlCharacters {
 
     // 获取OS系统指定的行分隔符, 跨OS平台
-    private static String lineSeparator = System.getProperty("line.separator");
+    private static final String LINE_SEPARATOR = System.getProperty("line.separator");
 
     public static void main(String[] args) {
-        System.out.println("a" + lineSeparator + "b");
+        System.out.println("a" + LINE_SEPARATOR + "b");
 
         String value = "aa\r\nbb";
         System.out.println(value);
-        System.out.printf(value.replaceAll(lineSeparator, " "));
+        System.out.printf(value.replaceAll(LINE_SEPARATOR, " "));
 
         String value2 = "cc\r\ndd";
-        if (value2.contains(lineSeparator)) {
+        if (value2.contains(LINE_SEPARATOR)) {
             System.out.println(value2);
         }
     }
 
     // 如果要将换行符号以字符串的方式显示出来，则必须添加转译符号
     public String escapeControlCharacters(String value) {
-        if (value.contains(lineSeparator)) {
-            switch (lineSeparator) {
+        if (value.contains(LINE_SEPARATOR)) {
+            switch (LINE_SEPARATOR) {
                 case "\r\n":
-                    // For windows
-                    return value.replaceAll(lineSeparator, " \\\\r\\\\n ");
+                    return value.replaceAll(LINE_SEPARATOR, " \\\\r\\\\n ");
                 case "\n":
-                    // For Unix/Linux & Mac (OS 10+)
-                    return value.replaceAll(lineSeparator, " \\\\n ");
+                    return value.replaceAll(LINE_SEPARATOR, " \\\\n ");
                 case "\r":
-                    // For Mac (OS 9-)
-                    return value.replaceAll(lineSeparator, " \\\\r ");
+                    return value.replaceAll(LINE_SEPARATOR, " \\\\r ");
                 default:
                     return value;
             }
         }
         return value;
+    }
+
+    // 用于测试目的，兼容不同平台的实际换行字符
+    static String getControlCharactersAsString() {
+        switch (LINE_SEPARATOR) {
+            case "\r\n":
+                return " \\r\\n "; // For windows
+            case "\n":
+                return " \\n ";  // For Unix/Linux & Mac (OS 10+)
+            case "\r":
+                return " \\r ";  // For Mac (OS 9-)
+            default:
+                return getLineSeparator();
+        }
+    }
+
+    public static String getLineSeparator() {
+        return LINE_SEPARATOR;
     }
 }
