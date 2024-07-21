ba6f2bbd30828c39c7f1be8f34be0ae0861fe659
tchen <tong.chen@lexisnexisrisk.com>
2024-07-21 18:17:23 +0200

    refresh thread pool and jvm thread destory

--- java-threads-locks/src/main/java/JavaConcurrencyDesign.java
+++ java-threads-locks/src/main/java/JavaConcurrencyDesign.java
@@ -1,5 +1,3 @@
-package multi_threading;
-
 import java.util.HashSet;
 import java.util.Set;
 
--- java-threads-locks/src/main/java/multi_threading/thread_pool/ThreadPoolDemo.java
+++ java-threads-locks/src/main/java/multi_threading/thread_pool/ThreadPoolDemo.java
@@ -6,10 +6,6 @@ import java.util.Random;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-// 使用线程池的理由
-// 1. 在某种场景下，使用线程池拥有更好的性能，尽可能的重用线程(线程缓存)
-// 2. 提高响应的速度，无需等待线程创建完成再执行
-// 3. 提供线程的统一分配和调控，只需要重点关注线程执行的task任务上
 public class ThreadPoolDemo {
 
     // 性能低：不停的创建新线程, 线程的创建和切换会造成性能低下
