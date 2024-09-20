3b738d37e708d82aba3e4076a22f96789d60f041
tchen <chen2015tong@gmail.com>
2024-09-20 16:18:30 +0200

    refresh enum class

--- src/JavaBasicOOP/BaseEnum/EnumTest.java
+++ src/JavaBasicOOP/BaseEnum/EnumTest.java
@@ -11,17 +11,17 @@ public class EnumTest {
         MANUAL // 2
     }
 
-    // TODO. Mock类型使用规范
-    // 1. Enum类型不能被实例化，没有构造器的概念
-    // 2. 枚举类型的字面名称就是它的name，可以通过name来获取特对特定的枚举类型。未定义的name会抛出异常
-    // 3. 枚举类型本身包含特定的内部方法.values() .name() .ordinal()
     public static void main(String[] args) {
+        // TODO. Enum类型不能被实例化，没有构造器的概念
         // SampleAction action0 = new SampleAction();  KO
         EnumDBType dbType = EnumDBType.MS_SQL_SERVER;
 
+        // TODO. Enum枚举类型包含两个特定的Static静态方法 .valueOf() .values() !!
+        // 枚举类型的字面名称就是它的name，可以通过name来获取特对特定的枚举类型。未定义的name会抛出异常
         // Returns the enum constant of this type with the specified name.
         // The string must match exactly an identifier used to declare an enum constant in this type.
         EnumDBType dbType1 = EnumDBType.valueOf("name");
+        EnumDBType[] enumDBTypes = EnumDBType.values();
 
         // Switch在比较时自动调用ordinal()方法比较值
         SampleAction action = SampleAction.valueOf("sample");
@@ -32,8 +32,7 @@ public class EnumTest {
             default -> System.out.println("error");
         }
 
-        // 调用默认的Internal方法
-        SampleAction[] sampleActions = SampleAction.values();
+        // TODO. 调用枚举类型默认包含的内部方法.name() .ordinal()
         System.out.println(action.name());
         System.out.println(action.ordinal());
     }
