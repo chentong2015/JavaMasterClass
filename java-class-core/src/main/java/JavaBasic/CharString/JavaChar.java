75e552c18ec39abd7c8d11c74a514f5d2022dfe8
tchen <chen2015tong@gmail.com>
2025-01-09 15:52:36 +0100

    update java Character API

--- java-class-core/src/main/java/JavaBasic/CharString/JavaChar.java
+++ java-class-core/src/main/java/JavaBasic/CharString/JavaChar.java
@@ -9,11 +9,14 @@ public class JavaChar {
     // Int值可转换成对于的Char值，再转换成字符串
     public static void main(String[] args) {
         char myChar = 'D';
-        char offsetChar = 'A' + 15; // 1. 使用常量进行偏移量计算, 隐式转换 !!
+
+        // 1. 使用常量进行偏移量计算, 隐式转换
+        char offsetChar = 'A' + 15;
         char constChar = 'A' + MY_INT;
 
+        // 2. 使用变量进行偏移计算，显式制转换
         int offset = 10;
-        char convertChar = (char) ('A' + offset); // 2. 使用变量进行偏移计算，显示强制转换 !!
+        char convertChar = (char) ('A' + offset);
         char convertChar2 = (char) (65 + offset);
 
         String value = "asa";
@@ -22,5 +25,9 @@ public class JavaChar {
         String str1 = String.valueOf((char)3);
         String str2 = String.valueOf((char)34);
         System.out.println(str2);
+
+        // TODO. Character API判断单个字符的类型
+        String str = "1test";
+        boolean isDigit = Character.isDigit(str.charAt(0));
     }
 }
