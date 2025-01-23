d55500794d0663fa90f0fc15eb3246efc8664e03
tchen <chen2015tong@gmail.com>
2025-01-23 12:51:36 +0100

    refresh java date time

---
+++ java-class-core/src/main/java/JavaDataTime/timestamp/TimestampProcessor.java
@@ -0,0 +1,71 @@
+package JavaDataTime.timestamp;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.*;
+
+public class TimestampProcessor {
+
+    static class Event {
+        public long doctorId;
+        public Timestamp timestamp;
+
+        public Event(long doctorId, Timestamp timestamp) {
+            this.doctorId = doctorId;
+            this.timestamp = timestamp;
+        }
+    }
+
+    // - 提供从DB表格查询出来的乱数据
+    // - 通过方法解析后，返回的结果能够之间被前端使用和显示
+    // 总的时间复杂度和Timestamp的数量有关
+    public HashMap<Long, List<Timestamp>> process(List<Event> events, Timestamp startTimestamp) {
+        // TreeMap<Long, List<Timestamp>> 根据Key值自然排序
+        HashMap<Long, List<Timestamp>> result = new HashMap<>();
+
+        // 1. Filtering + Group By ID 只截取七天有效时间
+        for (Event event : events) {
+            long doctorId = event.doctorId;
+            Timestamp timestamp = event.timestamp;
+            LocalDateTime nextSevenDateTime = startTimestamp.toLocalDateTime().plusDays(7);
+            if (timestamp.after(startTimestamp) && timestamp.toLocalDateTime().isBefore(nextSevenDateTime)) {
+                if (result.containsKey(doctorId)) {
+                    result.get(doctorId).add(timestamp);
+                } else {
+                    List<Timestamp> timestamps = new ArrayList<>();
+                    timestamps.add(timestamp);
+                    result.put(doctorId, timestamps);
+                }
+            }
+        }
+
+        // 2. Sort timestamp list
+        for (Map.Entry<Long, List<Timestamp>> entry : result.entrySet()) {
+            List<Timestamp> timestampList = entry.getValue();
+            Collections.sort(timestampList);
+
+            // 3. Refresh timestamp list
+            entry.setValue(refreshTimestampList(timestampList));
+        }
+        return result;
+    }
+
+    // Clean timestamp inside [timestamp, timestamp + 20m]
+    // 直接使用原timestampList列表来移除元素会造成O(N)时间复杂度
+    private List<Timestamp> refreshTimestampList(List<Timestamp> timestampList) {
+        List<Timestamp> timestampListNew = new ArrayList<>();
+        int index = 0;
+        while (index < timestampList.size()) {
+            Timestamp timestamp = timestampList.get(index);
+            timestampListNew.add(timestamp);
+
+            LocalDateTime nextDateTime = timestamp.toLocalDateTime().plusMinutes(20);
+            int nextIndex = index + 1;
+            while (nextIndex < timestampList.size() && timestampList.get(nextIndex).toLocalDateTime().isBefore(nextDateTime)) {
+                nextIndex++;
+            }
+            index = nextIndex;
+        }
+        return timestampListNew;
+    }
+}
