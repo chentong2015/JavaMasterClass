1247fd90b6c36a33e7aba8507d89cc7f8e60b209
tchen <chen2015tong@gmail.com>
2025-04-10 14:24:46 +0200

    update regular expression api

--- java-class-core/src/main/java/JavaRegularExpressions/RegularExpressionAPI.java
+++ java-class-core/src/main/java/JavaRegularExpressions/RegularExpressionAPI.java
@@ -1,17 +1,14 @@
 package JavaRegularExpressions;
 
-// TODO. 当参数不是正则表示式时, 使用.replace()来替换所有匹配的子字符串
-public class BaseRegularExpressionsAPI {
+public class RegularExpressionAPI {
 
     public static void main(String[] args) {
-        String value = "this test line this test";
-
         // TODO. 非正则形式: 替换所有匹配的子字符串
+        String value = "this test line this test";
         String result = value.replace("this", "OK");
         System.out.println(result);
 
-
-        // TODO. 正则表达式形式: 正则参数的API方法对性能有影响
+        // TODO. 正则表达式形式: 使用正则参数的API方法 => 对性能有影响
         boolean isMatch = value.matches(".*");
         System.out.println(isMatch);
 
@@ -22,19 +19,24 @@ public class BaseRegularExpressionsAPI {
         // 替换所有能够匹配的子字符串
         result = value.replaceAll("t.*t ", "as");
         System.out.println(result);
-
         result = value.replaceAll("[ ]?,", ",");
         System.out.println("---------");
+    }
 
-        // TODO. 按照正则来分隔字符串
-        String[] items = value.split("is");
-        for (String item: items) {
-            System.out.println(item);
-        }
+    // TODO. 正则表达式形式: Split分隔字符串
+    public static void testRegExSplit() {
+        // 如果分隔点后没有数据，则只保留开头子字符串
+        String info = "[item]";
+        String[] infos = info.split("item]");
+        System.out.println(infos.length); // "["
 
-        // TODO. 如果没有找到分割字符，则结果为原始字符串
+        String value = "this is a test";
+        String[] items = value.split("is"); // "th", " ", " a test"
+
+        // 如果没有找到分割字符，则结果为原始字符串
         String valueStr = "this is a test";
         String[] itemsStr = valueStr.split(":");
-        System.out.println(itemsStr.length); // 1
+        System.out.println(itemsStr.length); // "this is a test"
     }
+
 }
