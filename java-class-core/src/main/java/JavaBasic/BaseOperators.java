cf783577d7cae88d28e0f12355b40ef1bb694501
tchen <chen2015tong@gmail.com>
2025-04-25 16:56:32 +0200

    update java operators

--- java-class-core/src/main/java/JavaBasic/BaseOperators.java
+++ java-class-core/src/main/java/JavaBasic/BaseOperators.java
@@ -2,22 +2,39 @@ package JavaBasic;
 
 public class BaseOperators {
 
-    // TODO. 注意"前自增自减"和"后自增自减"符合的区别
-    // 前自增自减: 先移动，后判断
-    // 后自增自减: 先判断，后计算
     public static void main(String[] args) {
-        int[] num = {0, 1};
+        BaseOperators instance = new BaseOperators();
+        instance.testAndOrOperator(true, false, true);
+    }
+
+    private void testAndOrOperator(boolean bool1, boolean bool2, boolean bool3) {
+        // TODO. 先计算&&与, 结果再和bool3计算||或
+        if (bool1 && bool2 || bool3) {
+            System.out.println("True + True");
+            System.out.println("True + False + True");
+        }
+        // 先计算||或, 再计算&&与
+        if (bool1 && (bool2 || bool3)){
+            System.out.println("True + True");
+            System.out.println("True + False + True");
+        }
+    }
+
+    // TODO. 注意"前自增自减"和"后自增自减"符号
+    public void testAutoIncrement(int[] num) {
+        // TODO. if条件中先自增: 先自增，后取值判断
         if (++num[0] > 0) {
             System.out.println("check 0");
         }
-        // TODO. 在if条件判断中使用后自增: 先取值判断，后自增
+        System.out.println(num[0]);
+
+        // TODO. if条件中后自增: 先取值判断，后自增
         if (num[1]++ > 0) {
             System.out.println("check 1");
         }
-        System.out.println(num[0]);
         System.out.println(num[1]);
 
-        // TODO. 无论判断结果，一定会执行自增计算 !!
+        // TODO. 无论if判断的结果都会执行自增
         int start = 0;
         if (num[start++]++ > 1) {
             System.out.println("check start");
