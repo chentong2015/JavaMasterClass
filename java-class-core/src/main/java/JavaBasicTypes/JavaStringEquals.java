4de1fdad797338f496e45abc6687c8627c0eaf9a
tchen <chen2015tong@gmail.com>
2025-04-29 17:10:33 +0200

    update java string equals

--- java-class-core/src/main/java/JavaBasicTypes/JavaStringEquals.java
+++ java-class-core/src/main/java/JavaBasicTypes/JavaStringEquals.java
@@ -3,10 +3,10 @@ package JavaBasicTypes;
 // TODO: String Const Pool
 // 字符串常量池(JVM Heap堆中), 记录首次出现的实例引用
 // JVM uses string pools for allocation of string objects
-public class StringConstPool {
+public class JavaStringEquals {
 
-    // TODO. 字符串比较必须使用.equals()方法，必须比较字符串对象的内容 ！
-    // ==        tests for reference equality (whether they are the same object).
+    // TODO. 字符串比较必须使用.equals()方法，比较字符串对象的内容
+    // == tests for reference equality (whether they are the same object).
     // .equals() tests for value equality (whether they contain the same data).
     public static void main(String[] args) {
         String str1 = "value";
@@ -18,7 +18,7 @@ public class StringConstPool {
             System.out.println("It's same value");
         }
 
-        // TODO. null空值调用.equals()方法可能抛出异常
+        // TODO. null空对象不能调用.equals()方法
         String nullStr = null;
         if (nullStr.equalsIgnoreCase("ok")) {
             System.out.println("ok");
@@ -31,7 +31,8 @@ public class StringConstPool {
         System.out.println(s1.equals(s2)); // true  不可变类型的对象(包含)值相同
         System.out.println(s1 == s3);      // true  两个引用所引用的是常量池中相同的字符串对象
 
-        // 当调用intern方法时，如果字符串池中具有equal的字符串对象，则返回那个对象的引用
+        // 当调用intern方法时:
+        // 如果字符串池中具有equal的字符串对象，则返回那个对象的引用
         // 反之添加新字符串对象，然后其引用
         System.out.println(s1 == s1.intern()); // true  .intern()返回的是同一个引用
     }
