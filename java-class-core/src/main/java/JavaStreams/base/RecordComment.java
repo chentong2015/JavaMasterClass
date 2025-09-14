8ba110fc9cbf9ec8fd386f4e48e475f2bb1e99c5
chent5 <chen2015tong@gmail.com>
2025-09-14 18:14:42 +0200

    update java streams

---
+++ java-class-core/src/main/java/JavaStreams/base/RecordComment.java
@@ -0,0 +1,20 @@
+package JavaStreams.base;
+
+public class RecordComment {
+
+    private final String oid;
+    private final String content;
+
+    public RecordComment(String oid, String content) {
+        this.oid = oid;
+        this.content = content;
+    }
+
+    public String getOid() {
+        return oid;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
--- java-class-core/src/main/java/JavaStreams/features/StreamsGroupBy.java
+++ java-class-core/src/main/java/JavaStreams/features/StreamsGroupBy.java
@@ -1,23 +1,40 @@
 package JavaStreams.features;
 
 import JavaLambdaExpressions.base.Employee;
+import JavaStreams.base.RecordComment;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-// TODO. 对特定数据流按照classifier进行分组，然后收集分组结果
+// TODO. Stream流分组的操作
+// groupingBy() 根据特定的属性进行分组
+// reducing() averagingInt() 对每组没的数据进行计算处理
 public class StreamsGroupBy {
 
     private static void testStreamsReduce(List<Employee> employees) {
         // 计算每种性别的年龄总和
         Map<String, Integer> totalAgeByGender = employees.stream()
-                .collect(Collectors.groupingBy(Employee::getGender,
-                        Collectors.reducing(0, Employee::getAge, Integer::sum)));
+                .collect(Collectors.groupingBy(Employee::getGender, Collectors.reducing(0, Employee::getAge, Integer::sum)));
 
         // 计算每种性别的平均年龄
         Map<String, Double> averageAgeByGender = employees.stream()
-                .collect(Collectors.groupingBy(Employee::getGender,
-                        Collectors.averagingInt(Employee::getAge)));
+                .collect(Collectors.groupingBy(Employee::getGender, Collectors.averagingInt(Employee::getAge)));
+    }
+
+    // TODO. 将List<RecordComment>分组成Map<String, List<KzRecordComment>>
+    public static void main(String[] args) {
+        List<RecordComment> commentList = Arrays.asList(
+            new RecordComment("1", "Comment A"),
+            new RecordComment("2", "Comment B"),
+            new RecordComment("1", "Comment C"));
+        Map<String, List<RecordComment>> groupedMap = commentList.stream()
+                .collect(Collectors.groupingBy(RecordComment::getOid));
+
+        groupedMap.forEach((key, value) -> {
+            System.out.println("OID: " + key);
+            value.forEach(comment -> System.out.println("  " + comment.getContent()));
+        });
     }
 }
