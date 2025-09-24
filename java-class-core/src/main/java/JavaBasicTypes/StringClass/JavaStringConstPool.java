90163b7f4cd7ec3ec1e5ff2045428799940fc49c
chent5 <chen2015tong@gmail.com>
2025-09-24 11:10:06 +0200

    update java string const pool

---
+++ java-class-core/src/main/java/JavaBasicTypes/StringClass/JavaStringConstPool.java
@@ -0,0 +1,16 @@
+package JavaBasicTypes.StringClass;
+
+// TODO: String Const Pool
+// 字符串常量池(JVM Heap堆中), 记录首次出现的实例引用
+// JVM uses string pools for allocation of string objects
+public class JavaStringConstPool {
+
+    public static void main(String[] args) {
+        String s1 = "Programming";
+
+        // 当调用intern方法时:
+        // 如果字符串池中具有equal的字符串对象，则返回那个对象的引用
+        // 反之添加新字符串对象，然后其引用
+        System.out.println(s1 == s1.intern()); // true  .intern()返回的是同一个引用
+    }
+}
--- java-class-core/src/main/java/JavaBasicTypes/StringClass/JavaStringEquals.java
+++ java-class-core/src/main/java/JavaBasicTypes/StringClass/JavaStringEquals.java
@@ -1,9 +1,16 @@
 package JavaBasicTypes.StringClass;
 
-// TODO: String Const Pool
-// 字符串常量池(JVM Heap堆中), 记录首次出现的实例引用
-// JVM uses string pools for allocation of string objects
 public class JavaStringEquals {
+
+    // TODO. 注意字符串的两种比较方式
+    private static void compareString() {
+        String s1 = "Programming";
+        String s2 = new String("Programming");
+        String s3 = "Program" + "ming";
+        System.out.println(s1 == s2);      // false 比较的两个引用是不同的
+        System.out.println(s1.equals(s2)); // true  不可变类型的对象(包含)值相同
+        System.out.println(s1 == s3);      // true  两个引用所引用的是常量池中相同的字符串对象
+    }
 
     // TODO. 字符串比较必须使用.equals()方法，比较字符串对象的内容
     // == tests for reference equality (whether they are the same object).
@@ -18,22 +25,10 @@ public class JavaStringEquals {
             System.out.println("It's same value");
         }
 
-        // TODO. null空对象不能调用.equals()方法
+        // TODO. null空对象不能调用.equals()方法 !!
         String nullStr = null;
         if (nullStr.equalsIgnoreCase("ok")) {
             System.out.println("ok");
         }
-
-        String s1 = "Programming";
-        String s2 = new String("Programming");
-        String s3 = "Program" + "ming";
-        System.out.println(s1 == s2);      // false 比较的两个引用是不同的
-        System.out.println(s1.equals(s2)); // true  不可变类型的对象(包含)值相同
-        System.out.println(s1 == s3);      // true  两个引用所引用的是常量池中相同的字符串对象
-
-        // 当调用intern方法时:
-        // 如果字符串池中具有equal的字符串对象，则返回那个对象的引用
-        // 反之添加新字符串对象，然后其引用
-        System.out.println(s1 == s1.intern()); // true  .intern()返回的是同一个引用
     }
 }
