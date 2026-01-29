a76267083e9136b511d17ff887bfeddb3bfba3bb
chent5 <chen2015tong@gmail.com>
2026-01-29 17:38:57 +0100

    refresh project

--- java-new-features/src/main/java/main/features/java15/InstanceOfMatching.java
+++ java-new-features/src/main/java/main/features/java15/InstanceOfMatching.java
@@ -2,8 +2,8 @@ package main.features.java15;
 
 public class InstanceOfMatching {
 
-    // 在判断instanceof的同时完成类型转换, 避免强转时的类型异常
-    // 同时支持附加判断条件
+    // TODO. Java 15新特性: 在判断instanceof的同时完成类型转换
+    // 避免强转时的类型异常, 同时支持附加判断条件
     public void testPattern(Object obj) {
         if (obj instanceof String s && s.length() > 10) {
             // can use s here. No need to cast
